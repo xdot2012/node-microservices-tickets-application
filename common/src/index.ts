048beb706b6913185abe3095ce3dbdd197a3bcd3
Douglas Sales <douglasgsales@gmail.com>
2021-06-08 20:35:16 -0300

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -21,3 +21,5 @@ export * from './events/ticket-updated-event';
 export * from './events/types/order-status';
 export * from './events/order-created-event';
 export * from './events/order-cancelled-event';
+
+//test
